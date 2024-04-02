0baa9b1fded379649981c4373596ef174c2e0c61
Jonathan Pereira <jonathan14willian@gmail.com>
2024-04-01 23:27:30 -0300

    Removido modelo, controlador e serviço de livros

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { Livro } from './livros/book.model';
-import { LivroController } from './livros/books.controller';
-import { LivroService } from 'src/livros/books.service';
 import { ProdutoController } from './company/produto.controller';
 import { ProdutoService } from './company/produto.service';
 import { Produtos } from './company/produto.model';
@@ -18,13 +15,13 @@ import { Produtos } from './company/produto.model';
       username: 'root',
       password: 'atzmkl712',
       database: 'loja_eletronicos',
-      models: [Livro, Produtos],
+      models: [Produtos],
       autoLoadModels: true,
       synchronize: true,
     }),
-    SequelizeModule.forFeature([Livro, Produtos]),
+    SequelizeModule.forFeature([ Produtos]),
   ],
-  controllers: [AppController, LivroController, ProdutoController], 
-  providers: [AppService, LivroService, ProdutoService],
+  controllers: [AppController, ProdutoController], 
+  providers: [AppService, ProdutoService],
 })
 export class AppModule {}
